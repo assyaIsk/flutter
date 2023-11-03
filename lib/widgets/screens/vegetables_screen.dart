734f41f03113f44e88c64c8259bdc5a89f8ab917
assyaIsk <assyaall69@gmail.com>
2023-11-03 17:00:16 +0600

    tabs added

--- lib/widgets/screens/vegetables_screen.dart
+++ lib/widgets/screens/vegetables_screen.dart
@@ -3,8 +3,21 @@ import 'package:vegetables/widgets/category_list.dart';
 import 'package:vegetables/widgets/header.dart';
 import 'package:vegetables/widgets/vegetable_list.dart';
 
-class VegetablesScreen extends StatelessWidget {
+class VegetablesScreen extends StatefulWidget {
   const VegetablesScreen({super.key});
+
+  @override
+  State<VegetablesScreen> createState() => _VegetablesScreenState();
+}
+
+class _VegetablesScreenState extends State<VegetablesScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,6 +39,24 @@ class VegetablesScreen extends StatelessWidget {
             VegetableList(),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grid_view_rounded),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'User',
+          ),
+        ],
       ),
     );
   }
