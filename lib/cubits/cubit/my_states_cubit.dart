39067e4523f7036ff14a7a2da03a13c6e2a47abe
assyaIsk <assyaall69@gmail.com>
2024-01-30 23:55:23 +0600

    remove unused imports

--- lib/cubits/cubit/my_states_cubit.dart
+++ lib/cubits/cubit/my_states_cubit.dart
@@ -1,7 +1,4 @@
-import 'dart:ffi';
-
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:vegetables/data/categories_data.dart';
 import 'package:vegetables/data/vegetable_data.dart';
